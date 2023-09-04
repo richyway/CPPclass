fdb9672bbe056fee1c0169af4c354141eda006b1
richyway <richyway@ksvs.kh.edu.tw>
2023-09-04 13:08:43 +0800

    偵錯測試

---
+++ helloworldx5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello World"<<'1'<<endl;
+    cout<<"Hello World"<<'2'<<endl;
+    cout<<"Hello World"<<'3'<<endl;
+    cout<<"Hello World"<<'4'<<endl;
+    cout<<"Hello World"<<'5'<<endl;
+    return 0;
+}
